d1246963d659aa91d52b06c0bfd6cde55abba090
Alexandre Avanian <alexandre.avanian@gmail.com>
2015-08-23 16:03:07 +0900

    Configures bootstrap3

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,83 +3,76 @@
 from __future__ import unicode_literals
 import os
 
-# While developping:
+# Site generation behavior
 LOAD_CONTENT_CACHE = False
+RELATIVE_URLS = True
+DELETE_OUTPUT_DIRECTORY = True
 
+# Site configuration
 AUTHOR = 'Alexandre Avanian'
 SITENAME = '@/dev/null'
+SITE_SUBTEXT = 'Random rants & thoughts'
 SITEURL = 'http://aavanian.github.io'
 
-# Possibly useful to set as true but need more thinking
-USE_FOLDER_AS_CATEGORY = False
 PATH = 'content'
-STATIC_PATHS = ['images', 'pdfs']
-
-TIMEZONE = 'Asia/Hong_Kong'
-
-DEFAULT_LANG = 'en'
-# DEFAULT_DATE_FORMAT = '%a %d %B %Y'
-DEFAULT_DATE = 'fs'
-DEFAULT_CATEGORY = 'misc'
-
-THEME = 'themes/bootstrap3'
-
-# Alchemy theme configuration
-EXTRA_FAVICON = ''
-LICENSE_NAME = ''
-LICENCE_URL = ''
-MENU_ITEMS = {}
-META_DESCRIPTION = ''
-PAGES_ON_MENU = True
-CATEGORIES_ON_MENU = True
-TAGS_ON_MENU = True
-ARCHIVES_ON_MENU = False
-DISPLAY_PAGES_ON_MENU = True
-PROFILE_IMAGE = ''
-SHOW_ARTICLE_AUTHOR = False
-SITE_SUBTEXT = 'Random rants & thoughts'
-
-EMAIL_ADDRESS = 'alexandre.avanian@gmail.com'
-FB_ADDRESS = ''
-GITHUB_ADDRESS = 'https://www.github.com/aavanian'
-SO_ADDRESS = ''
-TWITTER_ADDRESS = 'http://www.twitter.com/aavanian'
-
-DISQUS_SITENAME = ''
-GOOGLE_ANALYTICS_DOMAIN = ''
-GOOGLE_ANALYTICS_ID = ''
-
-NOTEBOOK_DIR = 'notebooks'
-
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
-
-STATIC_PATHS = ['images', 'extra/robots.txt', 'extra/favicon.ico', '../.travis.yml', '../.gitmodules']
+STATIC_PATHS = ['images', 'extra/custom.css', 'extra/robots.txt', 'extra/favicon.ico', '../.travis.yml', '../.gitmodules']
 EXTRA_PATH_METADATA = {
     'extra/robots.txt': {'path': 'robots.txt'},
     'extra/favicon.ico': {'path': 'favicon.ico'},
     '../.travis.yml': {'path': '.travis.yml'},
-    '../.gitmodules': {'path': '.gitmodules'}
+    '../.gitmodules': {'path': '.gitmodules'},
+    'extra/custom.css': {'path': 'theme/css/custom.css'}
 }
+
+DEFAULT_LANG = 'en'
+TIMEZONE = 'Asia/Hong_Kong'
+DEFAULT_DATE_FORMAT = '%a %d %B %Y'
+DEFAULT_DATE = 'fs'
+DEFAULT_CATEGORY = 'misc'
+
+# Metadata stuff
+# Possibly useful to set as true but need more thinking
+USE_FOLDER_AS_CATEGORY = False
 
 # Plug-ins
 PLUGIN_PATHS = ['pelican-plugins']
 PLUGINS = ['liquid_tags.img', 'liquid_tags.video',
            'liquid_tags.youtube', 'liquid_tags.vimeo',
            'liquid_tags.include_code', 'liquid_tags.notebook']
-
-# for notebook rendering
+NOTEBOOK_DIR = 'notebooks'
 if not os.path.exists('_nb_header.html'):
     import warnings
     warnings.warn("_nb_header.html not found.  "
                   "Rerun make html to finalize build.")
 else:
     EXTRA_HEADER = open('_nb_header.html', encoding = 'utf-8').read()
-    # EXTRA_HEADER = open('_nb_header.html').read().decode('utf-8')
+
+# Theme
+THEME = 'themes/bootstrap3'
+BOOTSTRAP_THEME = 'Spacelab' # Yeti is also a good candidate
+PYGMENTS_STYLE = 'tango'
+CUSTOM_CSS = 'theme/css/custom.css'
+
+FAVICON = 'images/favicon.png'
+#SITELOGO = 'images/logo.png'
+#SITELOGO_SIZE = '128px'
+#HIDE_SITENAME = True
+BANNER = 'images/banner.png'
+BANNER_SUBTITLE = SITE_SUBTEXT
+PROFILE_IMAGE = ''
+DEFAULT_PAGINATION = 10
+CC_LICENSE = "CC-BY"
+
+DISPLAY_PAGES_ON_MENU = True
+DISPLAY_CATEGORIES_ON_MENU = True
+DISPLAY_TAGS_ON_MENU = False
+DISPLAY_ARCHIVES_ON_MENU = False
+MENU_ITEMS = {}
+
+DISPLAY_RECENT_POSTS_ON_SIDEBAR = True
+TAG_CLOUD_MAX_ITEMS = 10
+DISPLAY_TAGS_INLINE = True
+TAGS_URL = 'tags.html'
 
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
@@ -88,10 +81,32 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+TWITTER_USERNAME = 'aavanian'
+SOCIAL =   (('twitter', 'http://twitter.com/aavanian'),
+            ('linkedin', 'http://www.linkedin.com/in/alexandreavanian'),
+            ('github', 'http://github.com/aavanian'),
+            ('stackoverflow', 'http://stackoverflow.com/users/5190136/alexandre-avanian', 'stack-overflow'))
+SHARIFF = True
+SHARIFF_BACKEND_URL = ''
+SHARIFF_LANG = 'en'
+SHARIFF_SERVICES = "[&quot;twitter&quot;linkedin&quot;googleplus&quot;mail&quot;info&quot;]"
 
-DEFAULT_PAGINATION = 10
+DISQUS_SITENAME = ''
+GOOGLE_ANALYTICS = ''
+GOOGLE_ANALYTICS_DOMAIN = ''
+GOOGLE_ANALYTICS_UNIVERSAL = ''
+GOOGLE_ANALYTICS_UNIVERSAL_PROPERTY = ''
+GOOGLE_ANALYTICS_ID = ''
 
-# Uncomment following line if you want document-relative URLs when developing
-RELATIVE_URLS = True
+DISPLAY_ARTICLE_INFO_ON_INDEX = True
+SHOW_ARTICLE_AUTHOR = False
+SHOW_ARTICLE_CATEGORY = True
+SHOW_DATE_MODIFIED = True
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+FEED_ALL_RSS = ''
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
